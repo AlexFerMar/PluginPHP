e974dd45ba1ea743cfda1d24eb66f78f0e385e14
AlexFerMar <91198425+AlexFerMar@users.noreply.github.com>
2022-11-22 11:38:33 +0100

    Corregido código para que funcione

--- ModerationDatabase/ModerationDatabase.php
+++ ModerationDatabase/ModerationDatabase.php
@@ -21,41 +21,35 @@ function crearTabla()
     $table = $wpdb->prefix . 'correccionMalsonantes';
 
 
-    if (!($this->db->table_exists($table))) {
-
-
-        $sql = "CREATE TABLE $table (
+    $sql = "CREATE TABLE $table (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
         malsonante text NOT NULL,
         correccion text NOT NULL,
         PRIMARY KEY (id)
     ) $charset_collate;";
 
-        //Ejemplos de malsonantes
-        $sql1= "INSERT INTO $table (malsonante, correccion) VALUES ('dios', 'Panda Rojo')";
-        $sql2= "INSERT INTO $table (malsonante, correccion) VALUES ('mierda', 'popo')";
-        $sql3= "INSERT INTO $table (malsonante, correccion) VALUES ('pesetero', 'ahorrador')";
-        $sql4= "INSERT INTO $table (malsonante, correccion) VALUES ('idiota', 'bufon')";
-        $sql5= "INSERT INTO $table (malsonante, correccion) VALUES ('déspota', 'egoista')";
-        $sql6= "INSERT INTO $table (malsonante, correccion) VALUES ('chaquetero', 'indeciso')";
-        $sql7= "INSERT INTO $table (malsonante, correccion) VALUES ('muerto', 'en la otra vida')";
+    //Ejemplos de malsonantes
+    $sql1 = "INSERT INTO $table (malsonante, correccion) VALUES ('dios', 'Panda Rojo')";
+    $sql2 = "INSERT INTO $table (malsonante, correccion) VALUES ('mierda', 'popo')";
+    $sql3 = "INSERT INTO $table (malsonante, correccion) VALUES ('pesetero', 'ahorrador')";
+    $sql4 = "INSERT INTO $table (malsonante, correccion) VALUES ('idiota', 'bufon')";
+    $sql5 = "INSERT INTO $table (malsonante, correccion) VALUES ('déspota', 'egoista')";
+    $sql6 = "INSERT INTO $table (malsonante, correccion) VALUES ('chaquetero', 'indeciso')";
+    $sql7 = "INSERT INTO $table (malsonante, correccion) VALUES ('muerto', 'en la otra vida')";
 
 
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        //Creacion de la tabla
-        dbDelta($sql);
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    //Creacion de la tabla
+    dbDelta($sql);
 
-        //Insercion de los ejemplos
-        dbDelta($sql1);
-        dbDelta($sql2);
-        dbDelta($sql3);
-        dbDelta($sql4);
-        dbDelta($sql5);
-        dbDelta($sql6);
-        dbDelta($sql7);
-
-    }
-
+    //Insercion de los ejemplos
+    dbDelta($sql1);
+    dbDelta($sql2);
+    dbDelta($sql3);
+    dbDelta($sql4);
+    dbDelta($sql5);
+    dbDelta($sql6);
+    dbDelta($sql7);
 
 }
 
@@ -67,12 +61,22 @@ function moderacionMalsonante($text)
     global $wpdb;
     $table = $wpdb->prefix . 'correccionMalsonantes';
 
-    $malsonantes = dbDelta("SELECT malsonante FROM $table");
-    $correccion = dbDelta("SELECT correccion FROM $table");
+
+    $correccionMalsonantes = $wpdb->get_results("SELECT malsonante,correccion FROM $table", ARRAY_A);
+
+
+    $malsonantes = array();
+    $correccion = array();
+
+    foreach ($correccionMalsonantes as $row) {
+        $malsonantes[] = $row['malsonante'];
+        $correccion[] = $row['correccion'];
+
+    }
+
 
     return str_replace($malsonantes, $correccion, $text);
 }
 
 add_filter('the_content', 'moderacionMalsonante');
-
 
